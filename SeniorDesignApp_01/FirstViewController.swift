a92531ff582aec60390c70c85253f4e3d9e4f888
Steven <jiguangsen1998@126.com>
2019-11-22 21:34:31 -0500

    Initial Commit

---
+++ SeniorDesignApp_01/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SeniorDesignApp_01
+//
+//  Created by Steven on 11/22/19.
+//  Copyright © 2019 Steven. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SeniorDesignApp_01/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SeniorDesignApp_01
+//
+//  Created by Steven on 11/22/19.
+//  Copyright © 2019 Steven. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
